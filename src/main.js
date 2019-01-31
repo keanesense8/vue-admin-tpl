5b8442a9c35764fa5cca60133934e3f7f9b26675
keanesense8 <tengjuny0123@gmail.com>
2019-01-31 17:12:45 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import App from "./App.vue";
 import router from "./router/";
 import store from "./store";
 import "./registerServiceWorker";
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import 'font-awesome/css/font-awesome.css'
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'font-awesome/css/font-awesome.css';
+
+// import VueResource from 'vue-resource';
+// Vue.use(VueResource);
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
--- src/views/Migrate.vue
+++ src/views/Migrate.vue
@@ -41,36 +41,7 @@
           </mini-statistic>             
         </v-flex>   
         <!-- mini statistic  end -->   
-        <v-flex lg8 sm12 xs12>
-          <v-widget title="Self Chart" content-bg="white">
-            <v-btn icon slot="widget-header-action">
-              <v-icon class="text--secondary">refresh</v-icon>
-            </v-btn>
-            <div slot="widget-content">
-                <e-chart 
-                :path-option="[
-                  ['dataset.source', siteTrafficData],
-                  ['color', [color.lightBlue.base, color.green.lighten1]],
-                  ['legend.show', true],
-                  ['xAxis.axisLabel.show', true],
-                  ['yAxis.axisLabel.show', true],
-                  ['grid.left', '2%'],
-                  ['grid.bottom', '5%'],
-                  ['grid.right', '3%'],
-                  ['series[0].type', 'bar'],
-                  ['series[0].areaStyle', {}],
-                  ['series[0].smooth', true],
-                  ['series[1].smooth', true],
-                  ['series[1].type', 'bar'],
-                  ['series[1].areaStyle', {}],
-                ]"
-                height="400px"
-                width="100%"
-                >
-                </e-chart>     
-            </div>
-          </v-widget>  
-        </v-flex>
+        
         
       </v-layout>
     </v-container>
@@ -78,6 +49,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import API from '@/api';
 import EChart from '@/components/chart/echart';
 import MiniStatistic from '@/components/widgets/statistic/MiniStatistic';
@@ -98,110 +70,13 @@ export default {
   components: {
     VWidget,
     MiniStatistic,
-    ChatWindow,
     VCircle,
-    WeatherCard,
-    PostSingleCard,
-    PostListCard,
     ProfileCard,
     EChart,
-    BoxChart,
-    CircleStatistic,
     LinearStatistic,
-    PlainTable,
-    PlainTableOrder    
   },
   data: () => ({
-    color: Material,
-    selectedTab: 'tab-1',
-    linearTrending: [
-      {
-        subheading: 'Sales',
-        headline: '2,55',
-        caption: 'increase',
-        percent: 15,
-        icon: {
-          label: 'trending_up',
-          color: 'success'
-        },
-        linear: {
-          value: 15,
-          color: 'success'
-        }
-      },
-      {
-        subheading: 'Revenue',
-        headline: '6,553',
-        caption: 'increase',
-        percent: 10,
-        icon: {
-          label: 'trending_down',
-          color: 'error'
-        },
-        linear: {
-          value: 15,
-          color: 'error'
-        }
-      },
-      {
-        subheading: 'Orders',
-        headline: '5,00',
-        caption: 'increase',
-        percent: 50,
-        icon: {
-          label: 'arrow_upward',
-          color: 'info'
-        },
-        linear: {
-          value: 50,
-          color: 'info'
-        }
-      }
-    ],    
-    trending: [
-      {
-        subheading: 'Email',
-        headline: '15+',
-        caption: 'email opens',
-        percent: 15,
-        icon: {
-          label: 'email',
-          color: 'info'
-        },
-        linear: {
-          value: 15,
-          color: 'info'
-        }
-      },        
-      {
-        subheading: 'Tasks',
-        headline: '90%',
-        caption: 'tasks completed.',
-        percent: 90,
-        icon: {
-          label: 'list',
-          color: 'primary'
-        },
-        linear: {
-          value: 90,
-          color: 'success'
-        }
-      },        
-      {
-        subheading: 'Issues',
-        headline: '100%',
-        caption: 'issues fixed.',
-        percent: 100,
-        icon: {
-          label: 'bug_report',
-          color: 'primary'
-        },
-        linear: {
-          value: 100,
-          color: 'error'
-        }
-      },        
-    ]    
+    color: Material   
   }),
   computed: {
     activity () {
@@ -217,6 +92,25 @@ export default {
       return API.getLocation;
     }
   },
+  beforeMount() {
+    // this.$http.
+    setInterval(test,3000);
+    function test(){
+      var dt = new Date();
+      console.log("now: " + dt.getTime());
+      // this.$http.get('/backend/data')
+      //   .then(response => {
+
+      //   }, response => {
+
+      //   });
+      // this.$http.get('/backend').then(res => {});
+      // axios.get("/aaa")
+      //   .then(response => {});
+
+    }
+  }
+  
 
 };
 </script>
