b5fbf786e499318f1eb17aa0b9d427e83a9c1ab0
keanesense8 <tengjuny0123@gmail.com>
2019-02-02 13:33:19 +0800

    init the migrate dashboard

---
+++ src/api/migrateData.js
@@ -0,0 +1,15 @@
+const migrateData = {
+  id: "-1",
+  uploadedItemCount: 0,
+  uploadingItemCount: 0,
+  errorItemCount: 0,
+  uploadedData: "0.0GB",
+  speedDataByHour: "0.0",
+  totalProcess: "0.0",
+  runTime: "0 days, 0 hours, 0 minutes, 0 seconds",
+  startTime: "1970/0/0 00:00:00",
+  remainTime: "0 days, 0 hours, 0 minutes, 0 seconds",
+  estimateFinishTime: "1970/0/0 00:00:00",
+  totalData: "0.0"
+};
+export { migrateData };
--- src/views/Migrate.vue
+++ src/views/Migrate.vue
@@ -1,47 +1,94 @@
 <template>
   <div id="pageMigrate">
-    <v-container grid-list-xl fluid>
+    <v-container grid-list-xl fluid text-xs-center>
       
       <v-layout row wrap>
-        <!-- mini statistic start  icon="fa fa-facebook"-->
-       
-        <v-flex lg3 sm6 xs12>
-          <mini-statistic
-            icon=""
-            title="Uploaded"
-            v-bind:sub-title="num"
-            color="indigo"      
-          >
-          </mini-statistic>  
+        <v-flex xs12>
+          <v-card dark color="indigo">
+            <v-card-text class="px-0"><h1>剩余时间倒计时</h1></v-card-text>
+          </v-card>
         </v-flex>
-        <v-flex lg3 sm6 xs12>
-          <mini-statistic
-           
-            title="Uploading"
-            sub-title="10"
-            color="red"      
-          >
-          </mini-statistic>           
-        </v-flex>          
-        <v-flex lg3 sm6 xs12>
-          <mini-statistic
-           
-            title="Errors"
-            sub-title="1"
-            color="light-blue"      
-          >
-          </mini-statistic>            
-        </v-flex>        
-        <v-flex lg3 sm6 xs12>
-          <mini-statistic
-           
-            title="EXT"
-            sub-title="EXT"
-            color="purple"      
-          >
-          </mini-statistic>             
-        </v-flex>   
-        <!-- mini statistic  end -->   
+        <v-flex lg1>
+           <v-card color="blue lighten-3">
+             <v-card-text class="px-0"><h2>{{getDay}}</h2></v-card-text>
+           </v-card>
+        </v-flex>
+         <v-flex lg2 >
+           <v-card color="blue lighten-3" >
+             <v-card-text class="px-0"><h2>Days</h2></v-card-text>
+           </v-card>
+        </v-flex>
+         <v-flex lg1>
+           <v-card color="blue lighten-3">
+             <v-card-text class="px-0"><h2>{{getHour}}</h2></v-card-text>
+           </v-card>
+        </v-flex>
+         <v-flex lg2>
+           <v-card color="blue lighten-3">
+             <v-card-text class="px-0"><h2>Hours</h2></v-card-text>
+           </v-card>
+        </v-flex>
+         <v-flex lg1>
+           <v-card>
+             <v-card-text class="px-0"><h2>{{getMin}}</h2></v-card-text>
+           </v-card>
+        </v-flex>
+         <v-flex lg2>
+           <v-card>
+             <v-card-text class="px-0"><h2>Mins</h2></v-card-text>
+           </v-card>
+        </v-flex>
+         <v-flex lg1>
+           <v-card>
+             <v-card-text class="px-0"><h2>{{getSec}}</h2></v-card-text>
+           </v-card>
+        </v-flex>
+         <v-flex lg2>
+           <v-card>
+             <v-card-text class="px-0"><h2>Sec</h2></v-card-text>
+           </v-card>
+        </v-flex>
+        <v-flex xs3>
+          <v-card dark color="indigo">
+            <v-card-text class="px-0">已上传节目数：</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs3>
+          <v-card dark color="deep-purple darken-2">
+            <v-card-text class="px-0">上传中节目数：</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs3>
+          <v-card dark color="indigo">
+            <v-card-text class="px-0">错误的节目数：</v-card-text>
+          </v-card>
+        </v-flex>
+        <v-flex xs3>
+          <v-card dark color="deep-purple darken-2">
+            <v-card-text class="px-0">上传的数据量：</v-card-text>
+          </v-card>
+        </v-flex>
+        <!-- data of  -->
+        <v-flex xs3>
+          <v-card dark color="indigo">
+            <v-card-text class="px-0">{{ migrateData.uploadedItemCount}}</v-card-text>
+          </v-card>
+        </v-flex>
+         <v-flex xs3>
+          <v-card dark color="deep-purple darken-2">
+            <v-card-text class="px-0">{{ migrateData.uploadingItemCount }}</v-card-text>
+          </v-card>
+        </v-flex>
+         <v-flex xs3>
+          <v-card dark color="indigo">
+            <v-card-text class="px-0">{{ migrateData.errorItemCount }}</v-card-text>
+          </v-card>
+        </v-flex>
+         <v-flex xs3>
+          <v-card dark color="deep-purple darken-2">
+            <v-card-text class="px-0">{{ migrateData.uploadedData }}</v-card-text>
+          </v-card>
+        </v-flex>
         
         
       </v-layout>
@@ -52,6 +99,7 @@
 <script>
 import axios from 'axios'
 import API from '@/api';
+
 import SockJS from 'sockjs-client'
 import Stomp from 'stompjs'
 
@@ -70,7 +118,10 @@ import BoxChart from '@/components/widgets/chart/BoxChart';
 import ChatWindow from '@/components/chat/ChatWindow';
 import CircleStatistic from '@/components/widgets/statistic/CircleStatistic';
 import LinearStatistic from '@/components/widgets/statistic/LinearStatistic';
-import { send } from 'q';
+import FlipCountdown from 'vue2-flip-countdown'
+import vueAwesomeCountdown from 'vue-awesome-countdown'
+
+// import { send } from 'q';
 export default {
   components: {
     VWidget,
@@ -79,13 +130,39 @@ export default {
     ProfileCard,
     EChart,
     LinearStatistic,
+    FlipCountdown,
+    vueAwesomeCountdown,
   },
   data: () => ({
     color: Material ,
     num: "5",
-    stompClient: ''
+    stompClient: '',
+    migrateData: {
+      id: "-1",
+      uploadedItemCount: 0,
+      uploadingItemCount: 0,
+      errorItemCount: 0,
+      uploadedData: "0.0GB",
+      speedDataByHour: "0.0",
+      totalProcess: "0.0",
+      runTime: "0 days, 0 hours, 0 minutes, 0 seconds",
+      startTime: "1970/01/01 00:00:00",
+      remainTime: "0 days, 0 hours, 0 minutes, 0 seconds",
+      estimateFinishTime: "2020/01/01 00:00:00",
+      totalData: "0.0"
+    }
   }),
   computed: {
+    getDay(){
+        if(this.migrateData != null && this.migrateData.remainTime !=null)
+          return this.migrateData.remainTime.split(",")[0].replace("days" , "").trim()
+      },
+    getHour(){if(this.migrateData != null && this.migrateData.remainTime !=null) return this.migrateData.remainTime.split(",")[1].replace("hours" , "").trim()},
+    getMin(){if(this.migrateData != null && this.migrateData.remainTime !=null) return this.migrateData.remainTime.split(",")[2].replace("minutes" , "").trim()},
+    getSec(){if(this.migrateData != null && this.migrateData.remainTime !=null) return this.migrateData.remainTime.split(",")[3].replace("seconds" , "").trim()},
+    getUploadedItemCount(){
+      return this.migrateData.uploadedItemCount + "";
+    },
     activity () {
       return API.getActivity();
     },
@@ -131,39 +208,48 @@ export default {
       this.toConnect()
       // this.send()
       console.log("mounted")
+      console.log("migrate : " + this.migrateData)
   }, 
   methods:{
-     send() {
-        console.log("thisss : " + this.stompClient)
-        let messageJson = JSON.stringify({"name": 'message'});
-        this.stompClient.send("/app/hello", {}, messageJson);
-        console.log("/app/sendTest 你发送的消息:" + message);
+      send() {
+        // console.log("thisss : " + this.stompClient)
+        // let messageJson = JSON.stringify({"name": 'message'});
+        // this.stompClient.send("/app/hello", {}, messageJson);
+        // console.log("/app/sendTest 你发送的消息:" + message);
       },
-      onMessageReceived(r){
-        console.log(r)
+      onMessageReceived(message){
+        const self = this
+        console.log("receive from server : " + message.body)
+        var obj = JSON.parse(message.body);
+        /**
+         * {"id":null
+         * ,"uploadedItemCount":6323,"uploadingItemCount":0,"errorItemCount":53
+         * ,"uploadedData":"5717.030GB","speedDataByHour":"4.0176","totalProcess":"0.4516"
+         * ,"runTime":"0 days, 23 hours, 43 minutes, 10 seconds"
+         * ,"startTime":"2019/2/1 11:48:00","remainTime":"218 days, 19 hours, 23 minutes, 12 seconds\n"
+         * ,"estimateFinishTime":"2019/09/09 06:54:22\n","totalData":"1271441.579"}
+         */
+        // console.log(obj.uploadedItemCount)
+        self.migrateData = obj
+        // console.log(self.migrateData.runTime)
       },
       onConnected(){
           this.stompClient.subscribe('/topic/public', this.onMessageReceived);
 
         // Tell your username to the server
-          // this.stompClient.send("/app/chat.addUser"
-          //   ,{}
-          //   ,JSON.stringify({sender: username, type: 'JOIN'}))
-
-      
+          this.stompClient.send("/app/init"
+          ,{}
+          ,JSON.stringify({content: 'init'}))
       },
       onError(e){
-          console.log(e)
+          console.log("error:" + e)
       },
       
       toConnect() {
         // 建立连接对象（还未发起连接）
         var socket = new SockJS('http://localhost:8090/ws');
         this.stompClient = Stomp.over(socket);
-
         this.stompClient.connect({}, this.onConnected, this.onError);
-       
-        
       },
       
   },
@@ -191,4 +277,5 @@ export default {
   #input-usage .v-messages {
     border: 1px dashed rgba(0,0,0, .4);
   }
+
 </style>
